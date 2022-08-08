f0f66d0e4fd2a1eae7a7a53fb7ccf4f9de21a4e4
WinstonZhang1999 <zzwinston@hotmail.com>
2022-08-08 14:32:29 -0700

    Update tcp-echo-server.c
    some bug fixes on tcp-echo-server.c

--- tcp-echo-server.c
+++ tcp-echo-server.c
@@ -22,16 +22,16 @@ int main(int argc, char **argv)
 	unsigned short port = atoi(argv[1]);
 	
 	//creating a listening socket
-	int serversock;
+	int servsock;
 	const int con_q_size = 5;
 	const int buf_size = 10;
-	if ((serversock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	if ((servsock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		die("socket failed");
 
 	//construct local address structure
-	struct socketaddr_in servaddr;
+	struct sockaddr_in servaddr;
 	memset(&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
+	servaddr.sin_family= AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(port);
 
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
 	socklen_t clntlen;
 	struct sockaddr_in clntaddr;
 	
-	while(1){
+	while (1) {
 		fprintf(stderr, "waiting for client");
 	
 		clntlen = sizeof(clntaddr);
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
 		fprintf(stderr, "client ip: %s\n", inet_ntoa(clntaddr.sin_addr));
 		
 		while((r = recv(clntsock, buf, sizeof(buf), 0)) > 0){
-			*buf = touper(*buf);
+			*buf = toupper(*buf);
 			if(send(clntsock, buf, r, 0) != r){
 				fprintf(stderr, "ERR: send failed\n");
 				break;
@@ -73,99 +73,6 @@ int main(int argc, char **argv)
 			fprintf(stderr, "ERR: recv failed\n");
 		}
 
-		closed(clntsock);
-	}
-}
-	
-	int clntsock;
-	socklen_t clntlen;
-	struct sockaddr_in clntaddr;
-	
-	while(1){
-		fprintf(stderr, "waiting for client");
-	
-		clntlen = sizeof(clntaddr);
-	
-		if((clntsock = accept(servsock,
-					(struct sockaddr *) &clntaddr, &clntlen)) < 0)
-			die("accept failed");
-		
-		fprintf(stderr, "client ip: %s\n", inet_ntoa(clntaddr.sin_addr));
-		
-		while((r = recv(clntsock, buf, sizeof(buf), 0)) > 0){
-			*buf = touper(*buf);
-			if(send(clntsock, buf, r, 0) != r){
-				fprintf(stderr, "ERR: send failed\n");
-				break;
-			}
-		}
-
-		if(r < 0){
-			fprintf(stderr, "ERR: recv failed\n");
-		}
-
-		closed(clntsock);
-	}
-}
-	
-	int clntsock;
-	socklen_t clntlen;
-	struct sockaddr_in clntaddr;
-	
-	while(1){
-		fprintf(stderr, "waiting for client");
-	
-		clntlen = sizeof(clntaddr);
-	
-		if((clntsock = accept(servsock,
-					(struct sockaddr *) &clntaddr, &clntlen)) < 0)
-			die("accept failed");
-		
-		fprintf(stderr, "client ip: %s\n", inet_ntoa(clntaddr.sin_addr));
-		
-		while((r = recv(clntsock, buf, sizeof(buf), 0)) > 0){
-			*buf = touper(*buf);
-			if(send(clntsock, buf, r, 0) != r){
-				fprintf(stderr, "ERR: send failed\n");
-				break;
-			}
-		}
-
-		if(r < 0){
-			fprintf(stderr, "ERR: recv failed\n");
-		}
-
-		closed(clntsock);
-	}
-}
-	
-	int clntsock;
-	socklen_t clntlen;
-	struct sockaddr_in clntaddr;
-	
-	while(1){
-		fprintf(stderr, "waiting for client");
-	
-		clntlen = sizeof(clntaddr);
-	
-		if((clntsock = accept(servsock,
-					(struct sockaddr *) &clntaddr, &clntlen)) < 0)
-			die("accept failed");
-		
-		fprintf(stderr, "client ip: %s\n", inet_ntoa(clntaddr.sin_addr));
-		
-		while((r = recv(clntsock, buf, sizeof(buf), 0)) > 0){
-			*buf = touper(*buf);
-			if(send(clntsock, buf, r, 0) != r){
-				fprintf(stderr, "ERR: send failed\n");
-				break;
-			}
-		}
-
-		if(r < 0){
-			fprintf(stderr, "ERR: recv failed\n");
-		}
-
-		closed(clntsock);
+		close(clntsock);
 	}
 }
